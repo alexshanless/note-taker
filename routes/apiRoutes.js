a12b509be1d1e61538ee21b3444eff9b01dcab2b
Alex Bilba <alex.shanless@gmail.com>
2020-03-15 12:43:16 -0700

    updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,54 @@
-db = require("../db/db.json");
+const path = require('path');
+const fs = require('fs');
+const filePath = path.join(__dirname, '../db/db.json')
 
 module.exports = function (app) {
 
 
     app.get("/api/notes", function (req, res) {
-        res.json(db);
+
+        fs.readFile(filePath, 'utf8', function (err, db) {
+            if (err) res.sendStatus(500)
+
+            res.json(JSON.parse(db));
+        })
     });
 
 
     app.post("/api/notes", function (req, res) {
-
-
         const note = req.body;
-
-        note.id = new Date().getTime().toString();
-
+        note.id = new Date().getTime();
         console.log(note);
 
-        db.push(note);
-        res.json(note);
+        fs.readFile(filePath, 'utf8', function (err, db) {
+            if (err) res.sendStatus(500)
 
+            db = JSON.parse(db)
+            db.push(note);
+
+            fs.writeFile(filePath, JSON.stringify(db), function (err) {
+                if (err) res.sendStatus(500)
+
+                res.sendStatus(200)
+            })
+
+        })
     });
 
-    app.delete("/api/notes/:note", function (req, res) {
+    app.delete("/api/notes/:id", function (req, res) {
 
-        const note = req.body;
-        console.log(note);
+        fs.readFile(filePath, 'utf8', function (err, db) {
+            if (err) res.sendStatus(500)
 
-        db.splice(note, 1);
-        res.json(note);
+            db = JSON.parse(db).filter(note => note.id !== parseInt(req.params.id))
 
+            fs.writeFile(filePath, JSON.stringify(db), function (err) {
+                if (err) res.sendStatus(500)
+
+                res.sendStatus(200)
+            })
+
+        })
     });
 };
 
